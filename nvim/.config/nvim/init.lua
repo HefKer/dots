80ad60d1c89a146e1c6a4b2f0e0c7149c5c5403b
HefKer <nelsonjulioaviles@gmail.com>
2026-03-09 12:02:00 -0500

    Add nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Absolute number on current line, relative elsewhere
+vim.opt.number = true
+vim.opt.relativenumber = true
