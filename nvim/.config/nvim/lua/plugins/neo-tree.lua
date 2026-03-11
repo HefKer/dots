b77ee13f27ecca17af8181da90df16b25361c81d
HefKer <nelsonjulioaviles@gmail.com>
2026-03-10 23:07:05 -0500

    Update neotree config

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,18 +1,17 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    close_if_last_window = true,
     filesystem = {
       filtered_items = {
-	 visible = true,
-	 show_hidden_count = true,
-	 hide_dotfiles = false,
-	 hide_gitignored = true,
-	 hide_by_name = {
-     --    '.git',
-	   -- '.DS_Store',
-	   -- 'thumbs.db',
-	 },
-	never_show = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          --    '.git',
+        },
+        never_show = {
           '.git',
         },
       },
