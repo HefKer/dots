761189e7e9cdbce0fcb4a500d3945c1ef7e9af53
HefKer <nelsonjulioaviles@gmail.com>
2026-03-12 14:02:13 -0500

    Add fish and update wezterm configs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,19 +1,16 @@
--------------------------------------------------------------------------
--- Setup --
--------------------------------------------------------------------------
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- Import hotkeys from keys.lua
--- config.keys = require("keys")
+config.keys = require("keys")
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
 
--- Set fish as shell
--- config.default_prog = { "fish", "-l" }
+-- config.color_scheme = 'AdventureTime'
 
--------------------------------------------------------------------------
--- Styling --
--------------------------------------------------------------------------
+config.default_prog = { "fish", "-l" }
+
 config.color_scheme = "Catppuccin Frappe"
 config.window_background_opacity = 0.95
 local color_scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
@@ -26,9 +23,4 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.tab_max_width = 48
 
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
-
--- Finally, return the configuration to wezterm:
 return config
