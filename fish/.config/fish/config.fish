ba2db175e756d846f1dd9fe6f0f4182c16c177cf
hefker <nelsonjulioaviles@gmail.com>
2026-03-20 23:13:52 -0500

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,66 @@
-if status is-interactive
-# Commands to run in interactive sessions can go here
+# Login shell - Environment setup and one-time initializations
+if status is-login
+    # Tool initializations
+    zoxide init fish | source
+    starship init fish | source
+    fzf --fish | source
 end
+
+# Interactive shell - Shell behavior and user interface
+if status is-interactive
+    #fish_config theme choose catppuccin
+
+    # Aliases
+    alias cat="bat"
+    alias ls='eza -lh --group-directories-first --icons=auto'
+    alias lt='eza --tree --level=2 --long --icons --git'
+
+    # Abbreviations
+    abbr -a lsa 'ls -a'
+    abbr -a lta 'lt -a'
+
+    abbr -a c clear
+    abbr -a mv "mv -i" # Ask before overwriting
+
+    abbr -a n nvim
+    abbr -a lg lazygit
+    abbr -a g git
+    abbr -a gc git commit -m
+    abbr -a gcam git commit -am
+
+    abbr -a p python
+    abbr -a py python
+    abbr -a wifi impala
+    abbr -a taskman btop
+    abbr -a bt bluetui
+    abbr -a bluetooth bluetui
+
+    # Vim keybinds
+    set -g fish_key_bindings fish_vi_key_bindings
+
+    # Interactive functions
+    function run-ls-on-cd -v PWD
+        set current_repository (git rev-parse --show-toplevel 2> /dev/null)
+        if [ "$current_repository" ] && [ "$current_repository" != "$last_repository" ]
+            #onefetch
+        end
+        set -gx last_repository $current_repository
+        #command lsd -l
+        #command lsd -l --sort date
+    end
+
+    function cd
+        z $argv
+    end
+
+    function rm
+        command rm -Iv $argv # Ask when deleting more than 3 files, verbose
+    end
+
+    function fzf
+        nvim $(command fzf --preview "bat --style=numbers --color=always --line-range :500 {}")
+    end
+end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/hefker/.lmstudio/bin
