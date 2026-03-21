a0c671ee7ecef7c4ebd0e1fd4e753d80cb88ba85
hefker <nelsonjulioaviles@gmail.com>
2026-03-20 21:53:08 -0500

    Update nvim config

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      PATH = "skip",
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {},
+      automatic_installation = false,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {},
+        yamlls = {},
+        pyright = {},
+        nil_ls = {},
+        jsonls = {},
+      },
+    },
+  },
+}
