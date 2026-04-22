e6f8f241491d2a714905f78ae61fe73d5d4288ce
hefker <nelsonjulioaviles@gmail.com>
2026-04-21 22:42:23 -0500

    set up ranger as file picker

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,7 @@
+import shutil
+import os
+from themes import everforest
+
 """
 Reference:
 [Bread config](https://github.com/BreadOnPenguins/dots/blob/master/.config/qutebrowser/config.py)
@@ -7,6 +11,12 @@ Reference:
 
 userscript tutorial
 http://www.ii.com/qutebrowser-userscripts-on-windows/
+
+
+<PgDown>: completion-item-focus next-page
+<PgUp>: completion-item-focus prev-page
+
+q to quit (shift+esc in normal mode)
 """
 # from qutebrowser.api import interceptor
 
@@ -16,13 +26,10 @@ http://www.ii.com/qutebrowser-userscripts-on-windows/
 
 config.load_autoconfig()  # Load settings done via the GUI
 
-# Cosmetics
-
-from themes import everforest
+# --- Cosmetics ---
 everforest.set(c, "dark", "hard")  # options are dark/light and hard/medium/soft
 
-
-## Zen config (tabs on left)
+# Zen config (tabs on left)
 c.tabs.position = "left"
 # c.tabs.show = "always"
 config.set("tabs.show", "switching")
@@ -102,8 +109,16 @@ config.bind("U", "undo")
 config.bind(";r", "hint all right-click")
 
 config.bind("<Ctrl-h>", "history")
-
-config.bind(",ym", "yank inline [{title}]({url:pretty})")  # Markdown yank
+# c.editor.command = ["wezterm", "start", "--always-new-process", "--", "sh", "-c", "nvim {} && exit"]
+c.editor.command = ["/home/hefker/.local/bin/qute-editor", "{}"]
+# c.editor.command = ["wezterm", "start", "--always-new-process", "--", "nvim", "{}"]
+# c.editor.command = ["wezterm", "start", "--", "nvim", "{}"]
+# c.editor.command = ["wezterm", "start", "--always-new-process", "--", "sh", "-c", "nvim {} && exit"]
+config.bind(",Sf", "spawn firefox {url}")
+config.bind(",Sz", "spawn zen-browser {url}")
+config.bind(",Sb", "spawn brave {url}")
+config.bind("gD", "tab-clone")
+config.bind("ce", "config-edit")
 
 # Press 'tt' to toggle both the Tab bar and Status bar at once
 config.bind(
@@ -136,38 +151,28 @@ c.url.default_page = "https://github.com/hefker"
 """
 config.bind('=', 'cmd-set-text -s :open')
 config.bind('cc', 'hint images spawn sh -c "cliphist link {hint-url}"')
-config.bind('cs', 'cmd-set-text -s :config-source')
-config.bind('tH', 'config-cycle tabs.show multiple never')
-config.bind('sH', 'config-cycle statusbar.show always never')
-config.bind('T', 'hint links tab')
 config.bind('pP', 'open -- {primary}')
 config.bind('pp', 'open -- {clipboard}')
 config.bind('pt', 'open -t -- {clipboard}')
-config.bind('qm', 'macro-record')
 config.bind('<ctrl-y>', 'spawn --userscript ytdl.sh')
 config.bind('tT', 'config-cycle tabs.position top left')
 config.bind('gm', 'tab-move')
 """
+config.bind("cs", "config-source")
+config.bind("Q", "macro-record")
+config.bind("q", "fake-key <Escape>")
+config.bind("<Alt-Esc>", "fake-key <Escape>")
 # Focus binds
 # config.bind("xb", "config-cycle statusbar.show always never")
 # config.tind("xt", "config-cycle tabs.show always never")
 # config.bind("xx", "config-cycle statusbar.show always never")
 
 # Video binds
-"""
-check relevant sections:
-https://gitlab.com/dwt1/dotfiles/-/blob/master/.config/qutebrowser/config.py
+config.bind(",mf", "hint links spawn --detach mpv {hint-url}")
+config.bind(",mm", "spawn --detach mpv {url}")
+config.bind(",md", "hint links spawn st -e youtube-dl {hint-url}")
 
-need to set up mpv for this to work:
-config.bind('M', 'hint links spawn mpv {hint-url}')
-# config.bind('Z', 'hint links spawn st -e youtube-dl {hint-url}')
-config.bind('m', 'spawn mpv {url}')
-config.bind('M', 'hint links spawn mpv {hint-url}')
-"""
-config.bind(",vv", "hint links spawn --detach mpv {hint-url}")
-config.bind("<Alt-Esc>", "fake-key <Escape>")
-
-### Other
+# --- Other ---
 
 # Search engines
 c.url.open_base_url = True
@@ -178,6 +183,7 @@ c.url.searchengines = {
     "y": "https://www.youtube.com/results?search_query={}",
     "gh": "https://github.com/search?o=desc&q={}&s=stars",
     "github": "https://github.com/search?o=desc&q={}&s=stars",
+    "nix": "https://search.nixos.org/packages?channel=unstable&include_modular_service_options=1&include_nixos_options=1&query={}",
     # Google stuff
     "g": "https://www.google.com/search?q={}",
     "g.m": "https://www.google.com/maps?q={}",
@@ -188,6 +194,7 @@ c.url.searchengines = {
     "aw": "https://wiki.archlinux.org/?search={}",
     # Shopping
     "amazon": "https://www.amazon.com/s?k={}",
+    "amz": "https://www.amazon.com/s?k={}",
     "az": "https://www.amazon.com/s?k={}",
     "aliexpress": "https://www.aliexpress.com/wholesale?SearchText={}",
     "rb": "https://www.redbubble.com/shop/{}",
@@ -198,38 +205,71 @@ c.url.searchengines = {
 }
 
 
+# Use ranger as file picker
+# Shout out: [Qutebrowser + Ranger = Pure Awesome - YouTube](https://www.youtube.com/watch?v=ce2NOmTBWfo)
+def file_picker(chooser_flag):
+    wezterm = shutil.which("wezterm")
+    shell = shutil.which("fish") or shutil.which("bash")
+    ranger = shutil.which("ranger")
+
+    if not all([wezterm, shell, ranger]):
+        return None
+
+    return [
+        wezterm,
+        "start",
+        "--always-new-process",
+        "--class",
+        "ranger",
+        "--",
+        shell,
+        "-c",
+        f"{ranger} {chooser_flag} {{}}",
+    ]
+
+
+single = file_picker("--choosefile")
+multiple = file_picker("--choosefiles")
+
+if single and multiple:
+    config.set("fileselect.handler", "external")
+    config.set("fileselect.single_file.command", single)
+    config.set("fileselect.multiple_files.command", multiple)
+
+c.downloads.location.directory = "~/Downloads"
+
 # Privacy
 # privacy - adjust these settings based on your preference
 # config.set("completion.cmd_history_max_items", 0)
-# config.set("content.private_browsing", True)
+# config.set("content.private_browsing", True) # Global incognito
 # config.set("content.webgl", False, "*")
 # config.set("content.canvas_reading", False)
-# config.set("content.geolocation", False)
+# config.set("content.geolocation", False) # Block sites from asking for location
 # config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
 config.set("content.cookies.accept", "all")
 config.set("content.cookies.store", True)
 # config.set("content.javascript.enabled", False) # tsh keybind to toggle
 
 # Adblocking
-# You can also watch yt vids directly in mpv, see qutebrowser FAQ for how to do that.
-# If you want additional blocklists, you can get the python-adblock package, or you can uncomment the ublock lists here.
 c.content.blocking.enabled = True
-# c.content.blocking.method = 'adblock' # uncomment this if you install python-adblock
-# c.content.blocking.adblock.lists = [
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
-#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
+c.content.blocking.method = "both"  # "adblock" / "both"
+c.content.blocking.adblock.lists = [
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+    # "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+]
