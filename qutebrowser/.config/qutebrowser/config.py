a5fec6010b8ce6c16754e7926de7156b4eec70fd
hefker <nelsonjulioaviles@gmail.com>
2026-04-22 13:15:52 -0500

    adjust file picker to work with yazi or ranger based on selection

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -170,30 +170,59 @@ c.url.searchengines = {
 
 # Use ranger as file picker
 # Shout out: [Qutebrowser + Ranger = Pure Awesome - YouTube](https://www.youtube.com/watch?v=ce2NOmTBWfo)
-def file_picker(chooser_flag):
+def file_picker(manager="yazi"):
     wezterm = shutil.which("wezterm")
     shell = shutil.which("fish") or shutil.which("bash")
-    ranger = shutil.which("ranger")
 
-    if not all([wezterm, shell, ranger]):
-        return None
+    if manager == "yazi":
+        bin = shutil.which("yazi")
+        if not all([wezterm, shell, bin]):
+            return None, None
+        cmd = [
+            wezterm,
+            "start",
+            "--always-new-process",
+            "--class",
+            "yazi",
+            "--",
+            shell,
+            "-c",
+            f"{bin} --chooser-file={{}}",
+        ]
+        return cmd, cmd
 
-    return [
-        wezterm,
-        "start",
-        "--always-new-process",
-        "--class",
-        "ranger",
-        "--",
-        shell,
-        "-c",
-        f"{ranger} {chooser_flag} {{}}",
-    ]
+    elif manager == "ranger":
+        bin = shutil.which("ranger")
+        if not all([wezterm, shell, bin]):
+            return None, None
+        single = [
+            wezterm,
+            "start",
+            "--always-new-process",
+            "--class",
+            "ranger",
+            "--",
+            shell,
+            "-c",
+            f"{bin} --choosefile={{}}",
+        ]
+        multiple = [
+            wezterm,
+            "start",
+            "--always-new-process",
+            "--class",
+            "ranger",
+            "--",
+            shell,
+            "-c",
+            f"{bin} --choosefiles={{}}",
+        ]
+        return single, multiple
+
+    return None, None
 
 
-single = file_picker("--choosefile")
-multiple = file_picker("--choosefiles")
-
+single, multiple = file_picker("yazi")
 if single and multiple:
     config.set("fileselect.handler", "external")
     config.set("fileselect.single_file.command", single)
