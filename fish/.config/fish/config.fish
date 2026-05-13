e540e0f06ea62ed778d7cf63cf7487d54ea49c51
hefker <nelsonjulioaviles@gmail.com>
2026-05-12 20:21:03 -0500

    clean up fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ if status is-login
     starship init fish | source
     fzf --fish | source
     atuin init fish | source
+    set -gx MANPAGER 'bat -l man -p'
 end
 
 # Interactive shell - Shell behavior and user interface
@@ -30,7 +31,8 @@ if status is-interactive
     abbr -a c 'clear ; ls'
     abbr -a cl clear
     abbr -a mv "mv -iv" # Ask before overwriting
-    abbr -a cp "cp -v"
+    abbr -a cp "cp -iv"
+    abbr -a mkdir 'mkdir -pv'
 
     # CLI programs
     abbr -a n nvim
@@ -39,7 +41,6 @@ if status is-interactive
     abbr -a gs git status
     abbr -a ga git add
     abbr -a gc --set-cursor 'git commit -m "%"'
-    abbr -a gcm --set-cursor 'git commit -m "%"'
     abbr -a gcam --set-cursor 'git commit -am "%"'
     abbr -a gp git push
     abbr -a calc kalker
@@ -48,7 +49,6 @@ if status is-interactive
     abbr -a rg 'rg -i'
     abbr -a ns 'nix search nixpkgs'
 
-    abbr -a p python
     abbr -a py python
     abbr -a wifi impala
     abbr -a taskman btop
@@ -75,20 +75,24 @@ if status is-interactive
         nvim $(command fzf --preview "bat --style=numbers --color=always --line-range :500 {}")
     end
 
+    function __extract_yt_id
+        set url $argv[1]
+        if string match -q "*youtube.com*" $url
+            string replace -r '.*[?&]v=([^&]+).*' '$1' $url
+        else if string match -q "*youtu.be*" $url
+            string replace -r '.*youtu\.be/([^?]+).*' '$1' $url
+        else
+            echo $url
+        end
+    end
+
     function yt_summarize --description "Summarize a YouTube video transcript"
         if test (count $argv) -eq 0
             echo "Usage: yt_summarize <video_id_or_url>"
             return 1
         end
 
-        set video_id $argv[1]
-
-        # Extract video ID from URL if full URL is passed
-        if string match -q "*youtube.com*" $video_id
-            set video_id (string replace -r '.*[?&]v=([^&]+).*' '$1' $video_id)
-        else if string match -q "*youtu.be*" $video_id
-            set video_id (string replace -r '.*youtu\.be/([^?]+).*' '$1' $video_id)
-        end
+        set video_id (__extract_yt_id $argv[1])
 
         youtube_transcript_api $video_id --format json \
             | jq -r '.[][].text' \
@@ -102,21 +106,14 @@ if status is-interactive
             return 1
         end
 
-        set video_id $argv[1]
-
-        if string match -q "*youtube.com*" $video_id
-            set video_id (string replace -r '.*[?&]v=([^&]+).*' '$1' $video_id)
-        else if string match -q "*youtu.be*" $video_id
-            set video_id (string replace -r '.*youtu\.be/([^?]+).*' '$1' $video_id)
-        end
+        set video_id (__extract_yt_id $argv[1])
 
         set tmpfile /tmp/yt_transcript_$video_id.txt
 
         echo "Fetching transcript for $video_id..."
         youtube_transcript_api $video_id --format json \
             | jq -r '.[][].text' \
-            | tr '\n' ' ' \
-            > $tmpfile
+            | tr '\n' ' ' >$tmpfile
 
         if test $status -ne 0 -o ! -s $tmpfile
             echo "Failed to fetch transcript"
@@ -127,6 +124,4 @@ if status is-interactive
     end
 end
 
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/hefker/.lmstudio/bin
-set -gx MANPAGER 'bat -l man -p'
+fish_add_path /home/hefker/.lmstudio/bin
