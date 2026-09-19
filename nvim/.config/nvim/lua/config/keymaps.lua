d774bcbf734d24474e360ec94a4fa19bccec1a33
hefker <nelsonjulioaviles@gmail.com>
2026-09-19 14:34:12 -0500

    feat(nvim): add smart-splits for ctrl+hjkl pane navigation
    Replaces <leader>w h/j/k/l with <C-hjkl> to move and <C-A-hjkl> to
    resize, handing off to wezterm at the edge of the nvim window.
    
    The keymaps go in config/keymaps.lua rather than the plugin spec's
    `keys` so they load after LazyVim's own <C-hjkl> window bindings and
    actually win. Mods match the existing split_nav helper in wezterm's
    keys.lua, which already does the IS_NVIM passthrough.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- smart-splits: move/resize across nvim splits and wezterm panes alike.
+-- Mods match wezterm/.config/wezterm/keys.lua (split_nav): CTRL to move, CTRL+ALT to resize.
+local splits = function(fn)
+  return function()
+    require("smart-splits")[fn]()
+  end
+end
+
+vim.keymap.set("n", "<C-h>", splits("move_cursor_left"), { desc = "Go to Left Window/Pane" })
+vim.keymap.set("n", "<C-j>", splits("move_cursor_down"), { desc = "Go to Lower Window/Pane" })
+vim.keymap.set("n", "<C-k>", splits("move_cursor_up"), { desc = "Go to Upper Window/Pane" })
+vim.keymap.set("n", "<C-l>", splits("move_cursor_right"), { desc = "Go to Right Window/Pane" })
+
+vim.keymap.set("n", "<C-A-h>", splits("resize_left"), { desc = "Resize Window Left" })
+vim.keymap.set("n", "<C-A-j>", splits("resize_down"), { desc = "Resize Window Down" })
+vim.keymap.set("n", "<C-A-k>", splits("resize_up"), { desc = "Resize Window Up" })
+vim.keymap.set("n", "<C-A-l>", splits("resize_right"), { desc = "Resize Window Right" })
---
+++ nvim/.config/nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,8 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  lazy = false,
+  opts = {
+    -- Don't wrap around at the outermost split; hand off to wezterm instead.
+    at_edge = "stop",
+  },
+}
